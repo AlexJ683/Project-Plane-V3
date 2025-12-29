f7ca943e11c3e7e5bd9c0aadbbac3ddd5009e925
AlexJ683 <alex.johnson106@icloud.com>
2025-12-29 16:39:28 +0000

    linting testing

--- testing/test_backend.py
+++ testing/test_backend.py
@@ -4,8 +4,8 @@ import pytest
 from backend.Backend import app, get_db
 
 
-
 mock_session = MagicMock()
+
 
 def override_get_db():
     try:
@@ -13,36 +13,40 @@ def override_get_db():
     finally:
         pass
 
+
 app.dependency_overrides[get_db] = override_get_db
+
 
 @pytest.fixture
 def mock_db_session():
     return mock_session
 
+
 client = TestClient(app)
+
 
 def test_root(mock_db_session):
     response = client.post("/posts/", json = {"airline": "Placedholder",
-                                "flight_number": "PH000",
-                                "departure_city":"Placedholder",
-                                "departure_time": "Placedholder",
-                                "stops": 0,
-                                "arrival_time": "Placedholder",
-                                "arrival_city": "Placedholder",
-                                "travel_class": "Placedholder",
-                                "duration": "Placedholder",
-                                "days_left": 0,
-                                "price": 0})
+                                                "flight_number": "PH000",
+                                                "departure_city": "Placedholder",
+                                                "departure_time": "Placedholder",
+                                                "stops": 0,
+                                                "arrival_time": "Placedholder",
+                                                "arrival_city": "Placedholder",
+                                                "travel_class": "Placedholder",
+                                                "duration": "Placedholder",
+                                                "days_left": 0,
+                                                "price": 0})
     assert response.status_code == 201
     mock_db_session.add.assert_called()
     mock_db_session.commit.assert_called()
+
 
 def test_get_all_items(mock_db_session):
     response = client.get("/all_items/")
     assert response.status_code == 200
     mock_db_session.add.assert_called()
     mock_db_session.commit.assert_called()
-
 
 
 def test_get_item(mock_db_session):
